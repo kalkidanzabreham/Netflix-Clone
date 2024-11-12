a8911a3a0252794c65f992baf60cf5b673f4f72a
kalkidan <kalkidanabreham214@gmail.com>
2024-11-12 23:43:07 +0300

    aixos and request

---
+++ netflix-clone/src/Components/Utils/requests.jsx
@@ -0,0 +1,7 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+};
+export default requests;
